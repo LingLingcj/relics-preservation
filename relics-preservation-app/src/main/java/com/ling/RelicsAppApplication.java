f249ac10758eca8c8ffced6b9b8fe11019fde209
LingLingrj <3122973174@qq.com>
2025-06-26 22:21:06 +0800

    feat::项目初始化

--- relics-preservation-app/src/main/java/com/ling/RelicsAppApplication.java
+++ relics-preservation-app/src/main/java/com/ling/RelicsAppApplication.java
@@ -3,6 +3,9 @@ package com.ling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 31229
+ */
 @SpringBootApplication
 public class RelicsAppApplication {
 
